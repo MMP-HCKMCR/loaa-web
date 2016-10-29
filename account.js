55092c0da237af5c6d7a5ea08cfe09e16466f657
Fraham <frahamp@gmail.com>
2016-10-29 16:28:43 +0100

    Added the endpoint files

---
+++ account.js
@@ -0,0 +1,14 @@
+exports.retrieve = function(req, res){
+    // get an account from the database
+    res.json({ message: 'hooray! welcome to our ListOfMissingPeople!' });
+}
+
+exports.create = function(req, res){
+    // create a new account in the database
+    res.json({ message: 'hooray! welcome to our ListOfMissingPeople!' });
+}
+
+exports.update = function(req, res){
+    // update an new account in the database
+    res.json({ message: 'hooray! welcome to our ListOfMissingPeople!' });
+}
--- server.js
+++ server.js
@@ -4,9 +4,12 @@
 // =============================================================================
 
 // call the packages we need
+//var express = require('../..');
 var express    = require('express');        // call express
 var app        = express();                 // define our app using express
 var bodyParser = require('body-parser');
+var missing     = require('./missing');
+var account     = require('./account');
 
 // configure app to use bodyParser()
 // this will let us get the data from a POST
@@ -15,19 +18,13 @@ app.use(bodyParser.json());
 
 var port = process.env.PORT || 8080;        // set our port
 
-// ROUTES FOR OUR API
-// =============================================================================
-var router = express.Router();              // get an instance of the express Router
+module.exports = app;
 
-// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
-router.get('/', function(req, res) {
-    res.json({ message: 'hooray! welcome to our api!' });   
-});
-
-router.get('/ListOfMissingPeople', function(req, res) {
-    res.json({ message: 'hooray! welcome to our ListOfMissingPeople!' });   
-});
-
+app.get('/missing', missing.list);
+app.get('/account/:id', account.retrieve);
+app.post('/account', account.create);
+app.put('/account/:id', account.update);
+/*
 router.post('/MissingPersonSeen', function(req, res) {
     res.json({ message: 'hooray! welcome to our MissingPersonSeen!' });   
 });
@@ -42,13 +39,7 @@ router.delete('/DeleteAccount', function(req, res) {
 
 router.post('/Login', function(req, res) {
     res.json({ message: 'hooray! welcome to our Login!' });   
-});
-
-// more routes for our API will happen here
-
-// REGISTER OUR ROUTES -------------------------------
-// all of our routes will be prefixed with /api
-app.use('/api', router);
+});*/
 
 // START THE SERVER
 // =============================================================================
