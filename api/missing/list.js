aaccbda302e61baf3aceff2dd0de3c346c43a817
Guillaume Alpe <guillaumealpe@hotmail.com>
2016-10-29 20:21:26 +0100

    List now returns only missing people

--- api/missing/list.js
+++ api/missing/list.js
@@ -2,7 +2,7 @@ var MissingPerson = require('../../app/models/missingPerson');
 
 exports.list = function(req, res){
     // get the list of all missing people from the database
-    MissingPerson.find(function(err, allMissingPersons) {
+    MissingPerson.find({ status: "Missing" }, function(err, allMissingPersons) {
         if(err) {
             res.json(err);
         }
