e06c6092a592700af589890e57544a3539814dc0
Fraham <frahamp@gmail.com>
2016-10-29 23:52:25 +0100

    Makes the call to convert the police x and y to normal x and y

--- api/missing/list.js
+++ api/missing/list.js
@@ -2,42 +2,71 @@ var MissingPerson = require('../../app/models/missingPerson');
 var geolib = require('geolib');
 var request = {}
 
+var request2 = require('request');
+
+function getLatLong(person, callback) {
+    var myResponse = {};
+    console.log(person.posX27700 + ' ' + person.posY27700)
+    if (!person.latitude) {
+        request2.post(
+            'http://tasks.arcgisonline.com/arcgis/rest/services/Geometry/GeometryServer/project?inSR=27700&outSR=4326&geometries={%0D%0A"geometryType"+%3A+"esriGeometryPoint"%2C%0D%0A"geometries"+%3A+[%0D%0A+{"x"+%3A+' + person.posX27700 + '%2C+"y"+%3A+' + person.posY27700 + '}%0D%0A]%0D%0A}&transformation=&transformForward=true&f=pjson',
+            { json: { key: 'value' } },
+            function (error, response, body) {
+                if (!error && response.statusCode == 200) {
+                    console.log(body.geometries[0].x + " " + body.geometries[0].y)
+                    myResponse.latitude = body.geometries[0].x
+                    myResponse.longitude = body.geometries[0].y
+                    callback(myResponse);
+                }
+            }
+        );
+    } else {
+        myResponse.latitude = person.latitude
+        myResponse.longitude = person.longitude
+        callback(myResponse);
+    }
+}
+
+var length = 0;
+var count = 0;
+
+function loooooooop(allMissingPersons, callback) {
+    length = allMissingPersons.length;
+
+    count = 0;
+
+    for (i = 0; i < allMissingPersons.length; i++) {
+        (function (i) {
+            getLatLong(allMissingPersons[i], function (value) {
+                allMissingPersons[i].longitude = value.longitude
+                allMissingPersons[i].latitude = value.latitude
+                allMissingPersons[i].distance = geolib.getDistance(
+                    { latitude: request.body.latitude, longitude: request.body.longitude },
+                    { latitude: value.latitude, longitude: value.longitude }
+                );
+                count++;
+                if (count === length - 1) {
+                    callback("done");
+                }
+            });
+        })(i)
+    }
+}
+
 exports.list = function (req, res) {
     // get the list of all missing people from the database
     // needs to be post, latitude, longitude
     // add guid verify
-    console.log(req)
     request = req;
-    console.log(request.body)
 
     MissingPerson.find({ status: "Missing" }, function (err, allMissingPersons) {
         if (err) {
             res.json(err);
         }
 
-        for (i = 0; i < allMissingPersons.length; i++) {
-            if (!allMissingPersons[i].latitude) {
-                allMissingPersons[i].latitude = "53"
-                allMissingPersons[i].longitude = "-2"
-            }
-            console.log(request.body)
-            console.log(request.body.latitude)
-
-            allMissingPersons[i].distance = 0
-
-            /*allMissingPersons[i].distance = geolib.getDistance(
-                { latitude: request.body.latitude, longitude: 7.49347 },
-                { latitude: 51.5103, longitude: 7.49347 }
-            );*/
-
-            /*allMissingPersons[i].distance = geolib.getDistance(
-                { latitude: request.body.latitude, longitude: request.body.longitude },
-                { latitude: allMissingPersons[i].latitude, longitude: allMissingPersons[i].longitude }
-            );*/
-
-            //allMissingPersons[i].distance = "0"
-        }
-
-        res.json({ missing: allMissingPersons });
+        loooooooop(allMissingPersons, function (val) {
+            console.log("it has finished")
+            res.json({ missing: allMissingPersons });
+        });
     });
 };
