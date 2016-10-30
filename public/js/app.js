3bc9a7d0e7fe9369eae8ce2dc1186f2d718c2ff8
Guillaume Alpe <guillaumealpe@hotmail.com>
2016-10-30 02:20:47 +0000

    Added support for google maps

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,9 @@
-angular.module('loaaApp', ['ngRoute', 'appRoutes', 'MainCtrl', 'LoaaService']);
+angular.module('loaaApp', ['ngRoute', 'appRoutes', 'MainCtrl', 'LoaaService', 'uiGmapgoogle-maps'])
+.config(
+    ['uiGmapGoogleMapApiProvider', function(GoogleMapApiProviders) {
+        GoogleMapApiProviders.configure({
+            key: 'AIzaSyAVDlL7qpYutB6e5bNxvUs8W3dx4unPBAI',
+            v: '3.24', //defaults to latest 3.X anyhow
+            libraries: 'weather,geometry,visualization'
+        });
+    }]);
--- public/js/controllers/MainCtrl.js
+++ public/js/controllers/MainCtrl.js
@@ -1,5 +1,19 @@
-angular.module('MainCtrl', []).controller('MainController', function($scope) {
+angular.module('MainCtrl', [])
+    .controller('MainController', ['$scope', 'LoaaService', function ($scope, LoaaService) {
 
-    $scope.tagline = 'To the moon and back!';   
+        LoaaService.getMissing().then(function (res) {
+            $scope.missingPeople = res.data.missing;
+        });
+        $scope.tagline = 'To the moon and back!';
 
-});
+        // link map to html tag id
+        $scope.map = { 
+            center: { 
+                latitude: 53.4808, longitude: -2.2426 
+            }, 
+            zoom: 5 
+        };
+
+        $scope.markers = [];
+
+    }]);
