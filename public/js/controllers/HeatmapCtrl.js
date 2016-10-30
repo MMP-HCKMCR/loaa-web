7842a79e07480cc10b5805e9694f6dcb8f79c22e
Guillaume Alpe <guillaumealpe@hotmail.com>
2016-10-30 09:23:12 +0000

    Finished heatmap integration

--- public/js/controllers/HeatmapCtrl.js
+++ public/js/controllers/HeatmapCtrl.js
@@ -9,38 +9,69 @@ angular.module('HeatmapCtrl', [])
         //     zoom: 8
         // };
 
-        function MockHeatLayer(heatLayer) {
+        function HeatLayer(heatmapLayer) {
             // Adding 500 Data Points
             var map, pointarray, heatmap;
-
-            var taxiData = [
-                new google.maps.LatLng(37.782551, -122.445368),
-                new google.maps.LatLng(37.782745, -122.444586),
-                new google.maps.LatLng(37.782842, -122.443688),
-                new google.maps.LatLng(37.782919, -122.442815),
-                new google.maps.LatLng(37.782992, -122.442112),
-                new google.maps.LatLng(37.783100, -122.441461),
-                new google.maps.LatLng(37.783206, -122.440829),
-                new google.maps.LatLng(37.783273, -122.440324),
-                new google.maps.LatLng(37.783316, -122.440023),
-                new google.maps.LatLng(37.783357, -122.439794),
-                new google.maps.LatLng(37.783371, -122.439687)
-            ];
-
-
-            var pointArray = new google.maps.MVCArray(taxiData);
-            heatLayer.setData(pointArray);
+            $scope.pointCount = heatLayer.length;
+            var pointArray = new google.maps.MVCArray(heatLayer);
+            heatmapLayer.setData(pointArray);
         };
+
+        var heatLayer = [];
+
+        var addLatLng = function (lat, lng) {
+            heatLayer.push(new google.maps.LatLng(lat, lng));
+        };
+
+        var extractCoord = function (listOfMissingPeople) {
+            var lat;
+            var lng;
+            for (var i = 0; i < listOfMissingPeople.length; i++) {
+                var person = listOfMissingPeople[i];
+                lat = person.latitude;
+                lng = person.longitude;
+                if (lat && lng) {
+                    addLatLng(lat, lng);
+                }
+                lat = null;
+                lng = null;
+                var listLastSeen = person.lastSeen;
+                for (var j = 0; j < listLastSeen.length; j++) {
+                    var ls = listLastSeen[j];
+                    lat = ls.latitude;
+                    lng = ls.longitude;
+                    if (lat && lng) {
+                        addLatLng(lat, lng);
+                    }
+                    lat = null;
+                    lng = null;
+                }
+            }
+        };
+
+        var onSuccess = function (res) {
+            var missingPeople = res.data.missing;
+            $scope.countMissing = missingPeople.length;
+            extractCoord(missingPeople);
+        };
+
+        var onError = function (err) {
+            console.log("Call to get missing all failed");
+            console.log(err);
+        };
+
+        LoaaService.getAllMissing()
+            .then(onSuccess, onError);
 
         $scope.map = {
             center: {
-                latitude: 37.782551,
-                longitude: -122.445368
+                latitude: 53.4808,
+                longitude: -2.2426
             },
             zoom: 12,
             heatLayerCallback: function (layer) {
                 //set the heat layers backend data
-                var mockHeatLayer = new MockHeatLayer(layer);
+                var heatLayer = new HeatLayer(layer);
             },
             showHeat: true
         };
--- public/js/services/LoaaService.js
+++ public/js/services/LoaaService.js
@@ -8,12 +8,18 @@ angular.module('LoaaService', [])
                     "longitude": "-2.2426",
                     "latitude": "53.4808"
                 };
-                
                 return $http.post('/api/missing', data);
             },
 
-            getTop: function (){
+            getTop: function () {
                 return $http.get('/api/missing/top');
+            },
+            getAllMissing: function () {
+                var data = {
+                    "longitude": "-2.2426",
+                    "latitude": "53.4808"
+                };
+                return $http.post('/api/missing/all', data);
             }
         }
     }]);
